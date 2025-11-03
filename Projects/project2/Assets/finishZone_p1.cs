2a025e4261cfb6f725d9545800671497baf26415
borysb1 <borys.burzej@gmail.com>
2025-11-03 19:14:24 +0100

    Add files via upload

---
+++ Projects/project2/Assets/finishZone_p1.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using TMPro;
+
+
+public class finishZone_p1 : MonoBehaviour
+{
+    public TMP_Text WinText; // przypisz w Inspectorze
+    public float delayBeforeRestart = 3f;
+
+    private bool hasWon = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!hasWon && collision.CompareTag("Player"))
+        {
+            hasWon = true;
+            WinText.enabled = true; // pokaż napis
+            Debug.Log("xxxx");
+            
+
+            // opcjonalnie — zatrzymaj gracza
+            var rb = collision.GetComponent<Rigidbody2D>();
+            if (rb != null) rb.linearVelocity = Vector2.zero;
+
+            // opcjonalnie — restart po kilku sekundach
+            Invoke(nameof(RestartGame), delayBeforeRestart);
+        }
+    }
+
+    void RestartGame()
+    {
+        // proste ponowne załadowanie sceny
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+            UnityEngine.SceneManagement.SceneManager.GetActiveScene().name
+        );
+    }
+    
+}
