1b7747dc158b35feb66f1a93598d3dae63a2ae5f
borysb1 <borys.burzej@gmail.com>
2025-11-28 12:52:19 +0100

    Update cam_p1.cs

--- Projects/project2/Assets/cam_p1.cs
+++ Projects/project2/Assets/cam_p1.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target; // gracz
-    public float smoothSpeed = 5f; // szybkoœæ "doganiania"
-    public Vector3 offset; // przesuniêcie kamery wzglêdem gracza
+    public Transform target; 
+    public float smoothSpeed = 5f; 
+    public Vector3 offset; 
 
     void LateUpdate()
     {
         if (target == null) return;
 
-        // Docelowa pozycja kamery
+        
         Vector3 desiredPosition = target.position + offset;
 
-        // P³ynne przejœcie
+        
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
 
-        // Ustawienie nowej pozycji kamery
+       
         transform.position = smoothedPosition;
     }
+
 }
