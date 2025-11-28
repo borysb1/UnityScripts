826ff7f13c77b5719418043edc9d1425b45f8f4e
borysb1 <borys.burzej@gmail.com>
2025-11-28 12:55:27 +0100

    Create playerHealth.cs

---
+++ PlayerScripts/playerHealth.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("Ustawienia HP")]
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    [Header("UI")]
+    public TMP_Text hpText;
+
+    [Header("Respawn")]
+    public Transform spawnPoint;
+
+    [Header("Obra¿enia od kolców")]
+    public float damageCooldown = 1f; // przerwa miêdzy obra¿eniami
+    private float lastDamageTime = 0f;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHPText();
+    }
+
+    void Update()
+    {
+
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth < 0)
+            currentHealth = 0;
+
+        UpdateHPText();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void UpdateHPText()
+    {
+        if (hpText != null)
+            hpText.text = "HP: " + currentHealth;
+    }
+
+    void Die()
+    {
+        Debug.Log("Gracz zgin¹³!");
+        Respawn();
+    }
+    void Respawn()
+    {
+        transform.position = spawnPoint.position;
+        currentHealth = maxHealth;
+        UpdateHPText();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Trap"))
+        {
+            if(Time.time >= lastDamageTime + damageCooldown)
+            {
+                TakeDamage(50);
+                lastDamageTime = Time.time;
+                
+
+            }
+        }
+    }
+}
